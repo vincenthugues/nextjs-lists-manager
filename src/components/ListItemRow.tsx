f2711ef436c691eba7e697baeadf9e846961b833
Vincent Hugues <vincenthugues.dev@gmail.com>
2023-06-13 11:15:21 +0200

    feat: display checked/unchecked boxes for checkbox-enabled lists

--- src/components/ListItemRow.tsx
+++ src/components/ListItemRow.tsx
@@ -10,9 +10,23 @@ export default function ListItemRow({
   isChecked?: boolean;
 }) {
   return (
-    <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-      <h2 className={`mb-3 text-xl font-semibold`}>{name}</h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{notes}</p>
+    <div
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      style={{ display: 'flex', flexDirection: 'row' }}
+    >
+      <div
+        style={{
+          display: hasCheckbox ? 'inline' : 'none',
+          margin: '.2rem 1.5rem',
+          fontSize: '1.7rem',
+        }}
+      >
+        {isChecked ? '🗹' : '☐'}
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <h2 className={`mb-3 text-xl font-semibold`}>{name}</h2>
+        <p className={`m-0 max-w-[30ch] text-m opacity-90`}>{notes}</p>
+      </div>
     </div>
   );
 }
