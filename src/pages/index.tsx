40659103c06a1f4e7a24c3ae92fba24cc62237eb
Vincent Hugues <vincenthugues.dev@gmail.com>
2023-06-14 00:00:48 +0200

    feat: display dummy nav buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,14 @@ export const getServerSideProps: GetServerSideProps<{
   return { props: { lists } };
 };
 
+function NavButton({ label }: { label: string }) {
+  return (
+    <p className="left-0 top-0 flex justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 mx-2 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4 hover:bg-slate-900">
+      {label}
+    </p>
+  );
+}
+
 export default function Home({ lists }: { lists: List[] }) {
   if (!lists) {
     return <div>Error loading lists</div>;
@@ -24,10 +32,10 @@ export default function Home({ lists }: { lists: List[] }) {
     <main
       className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Item 1
-        </p>
+      <div className="z-10 w-auto max-w-5xl items-center justify-between text-m lg:flex mb-4">
+        {new Array(5).fill(null).map((_, index) => (
+          <NavButton key={index} label={`Item ${index + 1}`} />
+        ))}
       </div>
 
       <ListsPreviews lists={lists} />
