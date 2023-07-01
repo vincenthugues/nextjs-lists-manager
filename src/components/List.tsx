99d78ccd37f63153e704602a6e5196a270fb119f
Vincent Hugues <vincenthugues.dev@gmail.com>
2023-07-02 00:18:50 +0200

    feat: add ListItem creator placeholder

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,82 @@
 import ListItemRow from '@/components/ListItemRow';
 import { List } from '@/types/List';
 import { ListItem } from '@/types/ListItem';
+import { useState } from 'react';
+
+function ListItemCreator({}) {
+  const [isCreatorOpen, setIsCreatorOpen] = useState(false);
+  const [name, setName] = useState<string>();
+  const [notes, setNotes] = useState<string>();
+  const onSubmit = () => {
+    console.log({ name, notes });
+    setName('');
+    setNotes('');
+    setIsCreatorOpen(false);
+  };
+  const isFormValid = !!name || !!notes;
+
+  if (!isCreatorOpen) {
+    return (
+      <button
+        onClick={() => {
+          setIsCreatorOpen(true);
+        }}
+        style={{ margin: '3rem', fontSize: '1.5rem' }}
+      >
+        + Add item
+      </button>
+    );
+  }
+
+  return (
+    <form
+      onSubmit={(e) => {
+        onSubmit();
+        e.preventDefault();
+      }}
+      style={{ display: 'flex', flexDirection: 'column', margin: '3em' }}
+    >
+      <div style={{ margin: '.5em' }}>
+        Name
+        <input
+          type="text"
+          id="name"
+          onChange={({ target: { value } }) => {
+            setName(value);
+          }}
+          style={{ marginLeft: '.5em' }}
+        />
+      </div>
+      <div style={{ margin: '.5em' }}>
+        Notes
+        <input
+          type="text"
+          id="notes"
+          onChange={({ target: { value } }) => {
+            setNotes(value);
+          }}
+          style={{ marginLeft: '.5em' }}
+        />
+      </div>
+      <button
+        type="submit"
+        disabled={isFormValid === false}
+        style={{ fontSize: '1rem' }}
+      >
+        ✔️ Create
+      </button>
+      <button
+        type="reset"
+        onClick={() => {
+          setIsCreatorOpen(false);
+        }}
+        style={{ fontSize: '1rem' }}
+      >
+        ❌ Cancel
+      </button>
+    </form>
+  );
+}
 
 export default function List({
   list: { name, hasCheckboxes },
@@ -23,6 +99,7 @@ export default function List({
               hasCheckbox={hasCheckboxes}
             />
           ))}
+          <ListItemCreator />
         </div>
       </div>
     </>
